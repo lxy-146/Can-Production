e09b5e6b9dc015549d9ba49b391c26cd7d7a01cf
Spicalmx <1220873201@qq.com>
2021-10-12 23:42:01 +0800

    原型模式

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include"storehouse.h"
 #include"canfactory.h"
 #include"stapleobserve.h"
+#include"prototype.h"
 
 int main() {
 	OrderSubject* ordersubject=new OrderSubject();
@@ -10,5 +11,14 @@ int main() {
 	ordersubject->Attach(fruitobserve);
 	ordersubject->Attach(meatobserve);
 	ordersubject->SetOrder(10, 100);
+
+	//原型模式包装罐头
+	ConcretePrototype* fruitCanPrototype = new ConcretePrototype("Fruit can is already packed.");
+	fruitCanPrototype->show();
+	ConcretePrototype2* fruitCan1 = (ConcretePrototype2*)fruitCanPrototype->clone();
+	fruitCan1->show();
+	ConcretePrototype2* fruitCan2 = (ConcretePrototype2*)fruitCanPrototype->clone();
+	fruitCan2->show();
+
 	return 0;
 }
