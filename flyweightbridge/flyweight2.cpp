1112986973fc5081dc507cc9a7d1783a4d931534
coming1024 <57028337+coming1024@users.noreply.github.com>
2021-11-02 16:31:43 +0800

    新增注释
    新增注释

--- flyweightbridge/flyweight2.cpp
+++ flyweightbridge/flyweight2.cpp
@@ -1,21 +1,42 @@
 #include"flyweight2.h"
-
+/*
+*函数：MiddleCan::make
+*参数：null
+*功能：生产中等型号罐头
+*/
 void MiddleCan::make() {
 	kind->make();
 	cout << "middle can" << endl;
 }
-
+/*
+*函数：FishKind::make
+*参数：null
+*功能：生产鱼罐头
+*/
 void FishKind::make() {
 	cout << "produce fish ";
 }
-
+/*
+*函数：PorkKind::make
+*参数：null
+*功能：生产猪肉罐头
+*/
 void PorkKind::make() {
 	cout << "produce pork ";
 }
-
+/*
+*函数：BeefKind::make
+*参数：null
+*功能：生产牛肉罐头
+*/
 void BeefKind::make() {
 	cout << "produce beef ";
 }
+/*
+*函数：Factory
+*参数：string key
+*功能：如果没有需要的类型的罐头，则通过需求生产对应的罐头；如果已经有此类罐头，则返回此类罐头Can
+*/
 Can* Factory::GetFlyweights(string key) {
 	if (flyweights.count(key) == 0)
 	{
