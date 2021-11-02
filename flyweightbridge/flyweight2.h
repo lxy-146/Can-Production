04a31f11ecc148f7513f3cb3aec287b46efda50c
coming1024 <57028337+coming1024@users.noreply.github.com>
2021-11-02 16:24:42 +0800

    新增注释
    新增注释

--- flyweightbridge/flyweight2.h
+++ flyweightbridge/flyweight2.h
@@ -4,20 +4,20 @@
 #include"decoration.h"
 
 
-
+//生产鱼类罐头
 class FishKind :public Kind {
 public:
 	virtual void make();
 };
-
+//生产猪肉罐头
 class PorkKind :public Kind {
 	virtual void make();
 };
-
+//生产牛肉罐头
 class BeefKind :public Kind {
 	virtual void make();
 };
-
+//增加新的尺寸的中型罐头
 class MiddleCan :public Package {
 public:
 	MiddleCan(Kind* k) :kind(k) {}
@@ -25,7 +25,7 @@ public:
 private:
 	Kind* kind;
 };
-
+//通过哈希表匹配需要生产或管理的罐头类型
 class Factory {
 public:
 	Factory(){}
