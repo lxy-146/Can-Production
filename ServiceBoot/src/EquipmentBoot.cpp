e6a205660259bf0155a54114708590d559948a90
Hylissang <1916297575@qq.com>
2021-11-03 21:04:58 +0800

    修改EquipmentBoot注释

--- ServiceBoot/src/EquipmentBoot.cpp
+++ ServiceBoot/src/EquipmentBoot.cpp
@@ -29,6 +29,8 @@ void EquipmentBoot::Boot()
 
 /*
 * Function:Interpreter Pattern
+*			生产设备验证，判断将要启动的Equipment是否被支持，
+*			生产设备名称在验证成功后即可启动。
 */
 void EquipmentBoot::PowerVertificationOn()
 {
@@ -44,6 +46,8 @@ void EquipmentBoot::PowerVertificationOn()
 
 /*
 * Function:Lazy Initialization Pattern
+*			生产设备管理员的延迟初始化，当生产设备管理员多次被初始化时，将会返回
+*			生产设备管理员第一次被初始化时的已经存在的实例
 */
 void EquipmentBoot::EquipmentManagerOn()
 {
@@ -62,6 +66,8 @@ void EquipmentBoot::EquipmentManagerOn()
 
 /*
 * Function:Facade Pattern
+*			生产设备启动，客户端通过调用生产设备启动接口，生产设备内部子系统会依次启动
+*			而客户端只关心生产设备开机、关机的过程，并不需要了解生产设备内部子系统的启动过程。
 */
 void EquipmentBoot::EquipmentOn()
 {
@@ -76,6 +82,8 @@ void EquipmentBoot::EquipmentOn()
 
 /*
 * Function:Adapter Pattern
+*			适配器接收客户端的请求，适配器调用生产设备的特例方法。
+*			通过两层传递，实现了生产设备处理请求的适配
 */
 void EquipmentBoot::EquipmentAdapterOn()
 {
@@ -87,6 +95,7 @@ void EquipmentBoot::EquipmentAdapterOn()
 
 /*
 * Function:Adapter Pattern
+*			类适配器，通过私有继承生产设备的处理请求方法，以达到适配的目的
 */
 void EquipmentBoot::EquipmentClassAdapterOn()
 {
@@ -99,6 +108,8 @@ void EquipmentBoot::EquipmentClassAdapterOn()
 
 /*
 * Function:Adapter Pattern
+*			对象适配器，通过内部拥有一个生产设备成员对象，以调用处理请求的特例方法，
+*			达到适配的目的
 */
 void EquipmentBoot::EquipmentObjectAdapterOn()
 {
@@ -111,6 +122,9 @@ void EquipmentBoot::EquipmentObjectAdapterOn()
 
 /*
 * Function:BlackBoard Pattern
+*			黑板模式，存储罐头生产设备中的维修信息。生产设备管理员可以查看
+			在黑板中存储的维修信息，并对生产设备做相应的处理。黑板支持写入，读出，
+			展示黑板所有信息等操作。
 */
 void EquipmentBoot::BlackBoardOn()
 {
